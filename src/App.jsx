1777607e1b0046fc097bf25ae86e7aaba21dfb1b
Sachin kumar <149502766+asachin1610@users.noreply.github.com>
2025-05-04 22:30:39 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import './App.css'
 import HeroGradient from './components/heroSection/HeroGradient';
 import HeroMain from './components/heroSection/HeroMain';
-import NavbarMain from "./components/navBar/NavbarMain";
+import NavbarMain from "./components/navbar/NavbarMain";
 import SubHeroSection from './components/heroSection/SubHeroSection';
 import AboutMeMain from './components/aboutMeSection/AboutMeMain';
 import HelperSection from './components/HelperSection';
