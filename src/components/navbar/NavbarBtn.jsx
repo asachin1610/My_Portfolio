1b600340e976d42fa3de3e9ad63b8839dc5f8286
Sachin kumar <149502766+asachin1610@users.noreply.github.com>
2025-05-04 23:57:54 +0530

    NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { BsArrowDownRightCircleFill } from "react-icons/bs";
 
-const NavbarBtn = () => {
+const NavbarBtn = ({ className = "" }) => {
   return (
     <button 
       style={{ boxShadow: '0px 0px 20px 0px rgba(94, 206, 220, 0.5)' }}
-      className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan-300 border flex items-center gap-1 bg-gradient-to-r from-cyan-300 to-orange-400 hover:border-orange-400 hover:scale-110 transition-all duration-500 cursor-pointer hover:shadow-boxShadow">
-        Hire Me
-        <div className="hidden md:block">
-          <BsArrowDownRightCircleFill />
-        </div>
+      className={`px-4 py-2 rounded-full text-xl font-bold text-white border-cyan-300 border flex items-center gap-1 bg-gradient-to-r from-cyan-300 to-orange-400 hover:border-orange-400 hover:scale-110 transition-all duration-500 cursor-pointer hover:shadow-boxShadow ${className}`}
+    >
+      Hire Me
+      <div className="hidden md:block">
+        <BsArrowDownRightCircleFill />
+      </div>
     </button>
   );
 };
+
 
 export default NavbarBtn;
