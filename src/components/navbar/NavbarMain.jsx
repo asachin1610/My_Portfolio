27272ef0bdf6330173b6b59783feddf0404c0bd1
Sachin kumar <149502766+asachin1610@users.noreply.github.com>
2025-05-05 00:09:15 +0530

    NavbarMain.jsx

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -5,31 +5,26 @@ import NavbarBtn from './NavbarBtn';
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavbarMain = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => {
+  const [menuOpen,setMenuOpen]=useState(false);
+  const toggleMenu = ()=>{
     setMenuOpen(!menuOpen);
   };
-
   return (
     <nav className='max-w-[1300px] mx-auto px-4 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2'>
-      <div className="flex justify-between w-full max-w-[1200px] ms-auto bg-amber-950 items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-orange-400">
-        <NavbarLogo />
-
-        
-        <div className="lg:hidden">
-          <button onClick={toggleMenu} className="text-2xl p-3 border border-orange-400 rounded-full text-white">
+        <div className="flex justify-between w-full max-w-[1200px] ms-auto bg-amber-950 items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-orange-400">
+            <NavbarLogo/>
+            <div className={`${menuOpen ? "block" : "hidden"} lg:block`}>
+              <NavbarLinks/>
+            </div>
+            <NavbarBtn/>
+        </div>
+        <div className='flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full border-[0.5px] border-orange-400'>
+          <button 
+          className="text-2xl-1536px p-3 border border-orange-400 rounded-full text-red-100"
+          onClick={toggleMenu}>
             <GiHamburgerMenu />
           </button>
         </div>
-
-        
-        <div className={`${menuOpen ? "flex flex-col items-center" : "hidden"} lg:flex`}>
-          <NavbarLinks />
-        </div>
-
-        
-        <NavbarBtn className="hidden sm:flex" />
-      </div>
     </nav>
   );
 };
